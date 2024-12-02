5bd4c9d15176ba1b6773db3d9bb4dbf94ea84ae9
ryan yang <ryanyang1414@gmail.com>
2024-12-02 14:18:17 -0800

    Create python.py

---
+++ python.py
@@ -0,0 +1,22 @@
+import pymongo
+import os
+import sys
+import pprint
+
+def main():
+    connection_string = os.environ["MONGO_CONNECTION_STRING"]
+    db_name = os.environ["MONGO_DBNAME"]
+    
+    client = pymongo.MongoClient(connection_string)
+    db = client[db_name]
+    collection = db['birds'] #1. put the name of your collection in the quotes
+    
+    # Send a ping to confirm a successful connection
+    try:
+        client.admin.command('ping')
+        print("Pinged your deployment. You successfully connected to MongoDB!")
+    except Exception as e:
+        print(e)
+    
+if __name__=="__main__":
+    main()
